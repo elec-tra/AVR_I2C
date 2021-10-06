9cb87dc548f261ae32e4596faef825afa1345bc8
elec-tra <saranyan.stuttgart@gmail.com>
2021-10-07 00:23:44 +0200

    i2c initial test

--- i2c.h
+++ i2c.h
@@ -2,13 +2,13 @@
  * i2c.h
  *
  *  Created on: Oct 5, 2021
- *      Author: linuxus
+ *      Author: saranyan
  */
-
 #ifndef I2C_H_
 #define I2C_H_
 
 //-----Function Declarations------//
-
+void I2C_Init();
+void I2C_Start_Communication(uint8_t slave_addr);
 
 #endif /* I2C_H_ */
--- main.c
+++ main.c
@@ -7,8 +7,19 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdint.h>
 #include "common_preprocessors.h"
 #include "power_management.h"
+#include "usart0.h"
+#include "i2c.h"
+
+/*I2C*/
+#define Slave_Address 0b1101000
+
+/*USART0*/
+#define FOSC 16000000 					/*MCU Clock Speed*/
+#define BAUD 115200						/*Required Baud Rate*/
+#define MYUBRR ((FOSC/16/BAUD)-1)
 
 int main(void)
 {
@@ -17,10 +28,24 @@ int main(void)
 	DISABLE_ADC; ENABLE_I2C;			/*Disable ADC*/ /*ENABLE I2C*/
 	DISABLE_SPI; DISABLE_TIMER0;		/*Disable SPI*/ /*DISABLE TIMER0*/
 	DISABLE_TIMER1; DISABLE_TIMER2;		/*Disable TIMER1*/ /*DISABLE TIMER2*/
-	DISABLE_USART0;						/*Disable USART0*/
+	ENABLE_USART0;						/*Enable USART0*/
 	/*GPIO*/
 	SET_BIT(DDRB, PB5);					/*Pin PB5 as Output*/
 	CLEAR_BIT(PORTB, PB5);				/*Clear PB5 Bit*/
+	/*UART0*/
+	USART0_Init(MYUBRR);
+	/*I2C*/
+	I2C_Init();
+
+	/*Interrupt*/
+	SET_BIT(SREG, 7);					/*Enable Interrupt*/
+
+	/*USART0 Transmit Example*/
+	USART0_Send_Data("Serial Test Message...\n");
+	_delay_ms(10);
+
+	/*I2C Example*/
+	I2C_Start_Communication(Slave_Address);
 
 	//-----------Event loop----------//
 	while (1)
