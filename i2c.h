5172aa382bbabc5d694716e61181f3daf5cca5e8
elec-tra <saranyan.stuttgart@gmail.com>
2021-10-25 18:17:39 +0200

    optimised

--- i2c.h
+++ i2c.h
@@ -7,11 +7,16 @@
 #ifndef I2C_H_
 #define I2C_H_
 
-char data_buffer[19];
+#define I2C_COMPLETED 1
+#define I2C_NOT_COMPLETED 0
+
+//-----Variable Declarations------//
+char i2c_data_buffer[19];
 
 //-----Function Declarations------//
 void I2C_Init();
 void I2C_Start_Communication(uint8_t slave_addr);
+uint8_t I2C_Do_Communication();
 void I2C_Stop_Communication();
 
 #endif /* I2C_H_ */
--- main.c
+++ main.c
@@ -14,12 +14,15 @@
 #include "i2c.h"
 
 /*I2C*/
-#define Slave_Address 0b1101000
+#define SLAVE_ADDRESS 0b1101000
 
 /*USART0*/
 #define FOSC 16000000 					/*MCU Clock Speed*/
 #define BAUD 115200						/*Required Baud Rate*/
 #define MYUBRR ((FOSC/16/BAUD)-1)
+
+/*I2C Example - DS3231*/
+void Print_Seconds();
 
 int main(void)
 {
@@ -34,7 +37,8 @@ int main(void)
 	CLEAR_BIT(PORTB, PB5);				/*Clear PB5 Bit*/
 	/*UART0*/
 	USART0_Init(MYUBRR);
-
+	/*I2C*/
+	I2C_Init();
 	/*Interrupt*/
 	SET_BIT(SREG, 7);					/*Enable Interrupt*/
 
@@ -42,29 +46,45 @@ int main(void)
 	USART0_Send_Data("Serial Test Message...");
 	_delay_ms(5);
 
-	I2C_Init();
-	I2C_Start_Communication(Slave_Address);
+	/*Start I2C for first time*/
+	I2C_Start_Communication(SLAVE_ADDRESS);
 
 	//-----------Event loop----------//
 	while (1)
 	{
 		/*Blink LED Example*/
 		TOGGLE_BIT(PORTB, PB5);
-		_delay_ms(1000);
 
-		/*I2C Example*/
-//		I2C_Init();
-//		I2C_Start_Communication(Slave_Address);
+		_delay_ms(50);
 
-		//I2C_Stop_Communication();
-//		char seconds[3];
-//		char string_pointer[10];
-//		Integer_to_Characters((uint8_t) (data_buffer[0] >> 4), string_pointer);
-//		seconds[0] = string_pointer[0];
-//		Integer_to_Characters((uint8_t) (data_buffer[0] & 0b00001111), string_pointer);
-//		seconds[1] = string_pointer[0];
-//		seconds[2] = '\0';
-//		USART0_Send_Data(seconds);
+		if(TWCR & (1 << TWINT))
+		{
+			if(I2C_Do_Communication() == I2C_COMPLETED)
+			{
+				I2C_Start_Communication(SLAVE_ADDRESS);
+
+				/*Print Seconds Example - DS3231*/
+				_delay_ms(1);			/*Just for USART0 complete its previous message*/
+				Print_Seconds();
+			}
+			else
+				;//Till not completed, so, wait
+		}
 	}
 	return(0);
+}
+
+void Print_Seconds()
+{
+	char seconds[3];
+	char string_pointer[3];
+	//BCD to Integer conversion
+	Integer_to_Characters((uint8_t) (i2c_data_buffer[0] >> 4), string_pointer);
+	seconds[0] = string_pointer[0];
+	//BCD to Integer conversion
+	Integer_to_Characters((uint8_t) (i2c_data_buffer[0] & 0b00001111), string_pointer);
+	seconds[1] = string_pointer[0];
+	seconds[2] = '\0';
+
+	USART0_Send_Data(seconds);
 }
