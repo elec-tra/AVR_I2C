f419bd97fb4e1c498889f7ea28b846b9954cec04
elec-tra <saranyan.stuttgart@gmail.com>
2021-10-25 18:17:39 +0200

    optimised

--- i2c.c
+++ i2c.c
@@ -24,6 +24,14 @@
 static volatile uint8_t slave_address = 0xFF;
 volatile char * p;
 
+/*
+ * Function: Initialize I2C(i.e TWI) Peripheral
+ * --------------------------------------------
+ * Set and Configure registers of I2C
+ * peripheral for Master transmit and receive.
+ *
+ * returns: It returns nothing.
+ */
 void I2C_Init()
 {
 	//-------Set SCl Frequency-------//
@@ -52,111 +60,133 @@ void I2C_Init()
 	SET_BIT(TWCR, TWEN);				/*Enable TWI*/
 }
 
+/*
+ * Function: Starts I2C(i.e TWI) Peripheral
+ * ----------------------------------------
+ * Assigns a character pointer to I2C Data
+ * buffer variable(i.e i2c_data_buffer)
+ *
+ * slave_addr: 7-bit address of target slave
+ *
+ * returns: It returns nothing.
+ */
 void I2C_Start_Communication(uint8_t slave_addr)
 {
-	p = data_buffer;
-	SET_BIT(TWCR, TWIE);				/*Enable Interrupt*/
+	p = i2c_data_buffer;
+
 	SET_BIT(TWCR, TWSTA);				/*Give Start Signal*/
 	SET_BIT(TWCR, TWINT);				/*Ready for New Job*/
 	slave_address = slave_addr;
 }
 
-ISR(TWI_vect)
+/*
+ * Function: Executes communication between master and slave
+ * ---------------------------------------------------------
+ * This makes decision based on I2C status. I2C status can be
+ * determined with TWSR register.
+ *
+ * returns: I2C Communication status. 1 or 0
+ * 			Note: "1" means I2C Communication was successful
+ * 				  "0" means I2C Communication is not completed
+ * 				   yet
+ */
+uint8_t I2C_Do_Communication()
 {
+	//-------Debug TWI Status-------//
+	/*
+	 * This is just for debug I2C status.
+	 * So, The following three lines can
+	 * be commented if debug is not
+	 * needed
+	 */
 	char * debug_message;
 	USART0_Send_Data(Debug_Byte((uint8_t) TWSR, debug_message));
-	CLEAR_BIT(TWCR, TWIE);
-	_delay_ms(2000);
-	SET_BIT(TWCR, TWIE);
+	_delay_ms(4);
 
 	//-------Mater Transmitter------//
 	if(TWSR == 0x08)					/*Start Signal Sent Successfully*/
 	{
+		//-------Check TWI Status-------//
+		/*
+		 * The following line(USART0_Send_
+		 * Data("Step 1");) is just for
+		 * checking status of I2C.It can
+		 * be if debug is not needed
+		 */
 		USART0_Send_Data("Step 1");
 
 		TWDR = WRITE_OPERATION(slave_address);
+										/*Configure the slave for writing into slave*/
 		CLEAR_BIT(TWCR, TWSTA);			/*No more Start Signal needed*/
-		SET_BIT(TWCR, TWINT);			/*Continue the Job*/
 	}
 	if(TWSR == 0x18)					/*SLA+W has been transmitted Successfully*/
 	{
 		USART0_Send_Data("Step 2");
-		_delay_ms(100);
 
 		TWDR = 0x00;					/*Set Slave Address pointer to address 0x00*/
-		SET_BIT(TWCR, TWINT);			/*Continue the Job*/
 	}
 	if(TWSR == 0x28)					/*Data  byte  has  been  transmitted Successfully*/
 	{
 		USART0_Send_Data("Step 3");
 
 		SET_BIT(TWCR, TWSTA);			/*Give Repeated Start Signal*/
-		SET_BIT(TWCR, TWINT);			/*Continue the Job*/
 	}
 	if(TWSR == 0x10)					/*Repeated Start Signal sent Successfully*/
 	{
 		USART0_Send_Data("Step 4");
 
 		TWDR = READ_OPERATION(slave_address);
+										/*Configure the slave for reading from slave*/
 		CLEAR_BIT(TWCR, TWSTA);			/*No more Start Signal needed*/
-		SET_BIT(TWCR, TWINT);			/*Continue the Job*/
 	}
 
 	//--------Mater Receiver--------//
-	if(TWSR == 0x40)
+	if(TWSR == 0x40)					/*Slave configure as transmitter successfully*/
 	{
 		USART0_Send_Data("Step 5");
 
-		SET_BIT(TWCR, TWEA);			/*Send Acknowledge*/
-		SET_BIT(TWCR, TWINT);			/*Continue the Job*/
+		SET_BIT(TWCR, TWEA);			/*Enable Acknowledge for upcomming data*/
 	}
-	if(TWSR == 0x50)
+	if(TWSR == 0x50)					/*Slave started transmitting data bytes*/
 	{
-		//Received a byte from slave
-		if((p - data_buffer) <= 16)
+		if((p - i2c_data_buffer) <= 16)	/*Receive all bytes except last byte - with acknowledgement*/
 		{
 			USART0_Send_Data("Step 6");
 
-			*p++ = TWDR;
-			SET_BIT(TWCR, TWINT);		/*Continue the Job*/
+			*p++ = TWDR;				/*Store received bytes to buffer*/
 		}
-		else if((p - data_buffer) == 17)
+		else if((p - i2c_data_buffer) == 17)
+										/*Receive last byte - without acknowledgement*/
 		{
 			USART0_Send_Data("Step 7");
 
-			CLEAR_BIT(TWCR, TWEA);		/*Send Not Acknowledge*/
-			*p++ = TWDR;
-			SET_BIT(TWCR, TWINT);		/*Continue the Job*/
+			CLEAR_BIT(TWCR, TWEA);		/*Send Not Acknowledge - To stop slave Data transmit*/
+			*p++ = TWDR;				/*Store last byte*/
 		}
 	}
 	if(TWSR == 0x58)
 	{
-		*p = '\0';
-		CLEAR_BIT(TWCR, TWSTO);			/*Send Stop Signal*/
-		CLEAR_BIT(TWCR, TWIE);			/*Disable Interrupt*/
+		USART0_Send_Data("Step 8");
+
+		*p = '\0';						/*Terminate buffer with null character*/
+		SET_BIT(TWCR, TWSTO);			/*Send Stop Signal*/
 		SET_BIT(TWCR, TWINT);			/*Continue the Job*/
+		return 1;						/*"1" means I2C Communication was successful*/
 	}
 
-	CLEAR_BIT(TWCR, TWIE);
+	SET_BIT(TWCR, TWINT);				/*Continue the Job*/
+	return 0;							/*"0" means I2C Communication is not completed yet*/
 }
+
+/*
+ * Function: Disables I2C(i.e TWI) Peripheral
+ * ------------------------------------------
+ * It disable TWI peripheral
+ *
+ * returns: It returns nothing.
+ */
 void I2C_Stop_Communication()
 {
 	//----------Disable TWI----------//
 	CLEAR_BIT(TWCR, TWEN);				/*Disable TWI*/
 }
-
-
-
-//-----Debug I2C with USART0----//
-/*
- * The following code lines are used for debug status messages
- * from I2C. The debug messages are printed via serial commun-
- * ication. Here, Interrupt is enabled for both I2C and USART0,
- * but only one ISR be executed at a time. So, we have to disa-
- * ble I2C interrupt temporally for execution usart0 interrupt.
- */
-//USART0_Send_Data(Byte_to_HexChar((uint8_t) TWSR, debug_message));
-//CLEAR_BIT(TWCR, TWIE);
-//_delay_ms(2000);
-//SET_BIT(TWCR, TWIE);
-
